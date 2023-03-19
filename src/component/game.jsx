b3673cacecd2ac5e8c321f2385548750b3f4f795
majedurwd <majedur.cse@gmail.com>
2023-03-20 00:46:19 +0600

    Game Winning Logic

--- src/component/game.jsx
+++ src/component/game.jsx
@@ -2,7 +2,25 @@ import React from "react";
 import Board from "./board";
 
 function calculateWinner(squares) {
-    return
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]  
+    ]
+
+    for (let i = 0; i < lines.length; i++){
+        const [a, b, c] = lines[i]
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a]
+        }
+    }
+
+    return null
 }
 
 class Game extends React.Component {
@@ -31,10 +49,14 @@ class Game extends React.Component {
     }
 
     render() {
+        const history = this.state.history
+        const current = history[this.state.stepNumber]
+        const winner = calculateWinner(current.squares)
+        console.log(winner)
         return (
             <div>
                 <h3>Game Component</h3>
-                <Board onClick={ this.handleClick } squares={this.state.history[0]} />
+                <Board onClick={ this.handleClick } squares={current.squares} />
             </div>
         )
 
